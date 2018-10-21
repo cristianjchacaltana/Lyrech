149ce5f08fb2e2542f6d0913ef8f62577769e6e6
Cristian Jeancarlo Chacaltana Fajardo <cristianjchacaltana@gmail.com>
2018-10-21 18:22:43 -0500

    Caracteristica Front construccion de la interfaz de usuario

---
+++ app/src/main/java/com/firebaseapp/lyrech/lyrech/front/FrontActivity.java
@@ -0,0 +1,15 @@
+package com.firebaseapp.lyrech.lyrech.front;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firebaseapp.lyrech.lyrech.R;
+
+public class FrontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+    }
+}
